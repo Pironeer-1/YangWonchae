90b98df512896357a4a4f3e3d340a057f6c09d90
ywonchae1 <ywonchae62@gmail.com>
2023-08-30 22:33:32 +0900

    01React 04컴포넌트 만들기
    function component

--- 01React/react-app/src/App.js
+++ 01React/react-app/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return <header>
+    <h1><a href="/">WEB</a></h1>
+  </header>
+}
+function Nav() {
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">javascript</a></li>
+    </ol>
+  </nav>
+}
+function Article() {
+  return <article>
+  <h2>Welcome</h2>
+  Hello, WEB
+</article>
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <h1><a href="/">WEB</a></h1>
-      </header>
-      <nav>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-          <li><a href="/read/3">javascript</a></li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
