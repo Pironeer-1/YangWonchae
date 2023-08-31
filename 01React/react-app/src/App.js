838999713c8cc77e34570c466a5f22d87e04d289
ywonchae1 <ywonchae62@gmail.com>
2023-08-31 12:23:20 +0900

    01React 10Delete

--- 01React/react-app/src/App.js
+++ 01React/react-app/src/App.js
@@ -109,12 +109,27 @@ function App() {
       }
     }
     contextControl = 
+    <>
     <li>
       <a href={"/update/"+id} onClick={event => {
         event.preventDefault();
         setMode('UPDATE');
       }}>Update</a>
     </li>
+    <li>
+      <button onClick={() => {
+        const newTopics = [];
+        for(let i = 0; i < topics.length; i++) {
+          if(topics[i].id !== id) {
+            newTopics.push(topics[i]);
+          }
+        }
+        setTopics(newTopics);
+        setMode('WELCOME');
+        setId(0);
+      }}>Delete</button>
+    </li>
+    </>
   } else if(mode === 'CREATE') {
     content = <Create onCreate={(_title, _body) => {
       const newTopic = {id: nextId, title: _title, body: _body};
