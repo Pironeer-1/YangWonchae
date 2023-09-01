84f137f10932c72261dcc7b0238a3a4661876631
ywonchae1 <ywonchae62@gmail.com>
2023-09-01 12:12:57 +0900

    React 09장 조건부 렌더링

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -10,6 +10,7 @@ import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -18,10 +19,11 @@ setInterval(() => {
     <React.StrictMode>
       {/* <Library />
       <Clock />
-      <CommentList /> */}
-      {/* <NotificationList /> */}
-      {/* <Accommodate /> */}
-      <ConfirmButton />
+      <CommentList />
+      <NotificationList />
+      <Accommodate />
+      <ConfirmButton /> */}
+      <LandingPage />
     </React.StrictMode>
   );
 }, 1000);
