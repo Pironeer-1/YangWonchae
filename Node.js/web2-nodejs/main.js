0e306897ec6cc8f1ea67ce664ce8ad8a3e04aa14
ywonchae1 <ywonchae62@gmail.com>
2023-09-02 15:59:25 +0900

    Node.js 01-19 App-오류 페이지와 홈페이지 구현

--- Node.js/web2-nodejs/main.js
+++ Node.js/web2-nodejs/main.js
@@ -4,35 +4,63 @@ const url = require('url');
 const app = http.createServer(function(request, response) {
     let _url = request.url;
     const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
     let title = queryData.id;
-    if(_url == '/') {
-        title = 'Welcome';
+
+    if(pathname === '/') {
+        if(queryData.id === undefined) {
+            const title = 'Welcome';
+            desctiption = 'Hello, Node.js';
+            const template = `
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${desctiption}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+        } else {
+            fs.readFile(`data/${queryData.id}.html`, 'utf8', function(err, desctiption) {
+                const title = queryData.id;
+                const template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2>${title}</h2>
+                <p>${desctiption}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
     }
-    if(_url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}.html`, 'utf8', function(err, desctiption) {
-        const template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${desctiption}</p>
-        </body>
-        </html>
-        `;
-        response.end(template);
-    });
 });
 app.listen(3000);
