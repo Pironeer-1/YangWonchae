0e9b3b8ade91736e4f1f42170d8aa4ed9e2047cf
ywonchae1 <ywonchae62@gmail.com>
2023-09-02 14:32:59 +0900

    Node.js 05-07 자바스크립트-변수

---
+++ Node.js/web2-nodejs/syntax/variable.js
@@ -0,0 +1,4 @@
+const a = 1;
+console.log(a);
+a = 2;
+console.log(a);
---
+++ Node.js/web2-nodejs/syntax/variable2.js
@@ -0,0 +1,3 @@
+const name = 'egoing';
+const letter = 'Lorem ipsum dolor sit amet, ' + name + 'consectetur adipiscing elit. Vestibulum auctor aliquam elit egestas scelerisque. Nunc at interdum arcu. Nunc viverra luctus nulla at efficitur. Vivamus vulputate ut sapien quis efficitur. Duis maximus tellus neque. Aenean ipsum eros, hendrerit eu nisi eu, ' + name + 'tristique elementum justo. Ut id tincidunt justo, vitae vehicula lectus. Integer ac malesuada ligula. Fusce pulvinar lectus vel lorem eleifend congue. Nunc molestie ipsum in massa commodo, sit amet iaculis quam lacinia. Sed in dolor ' + name + 'molestie elit faucibus hendrerit. Maecenas et sapien luctus, laoreet mauris sed, rhoncus mi. Sed tincidunt cursus urna, convallis dignissim magna hendrerit sit amet.';
+console.log(letter);
