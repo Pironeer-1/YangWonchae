d09753a65114b644898fde295f6a6408b958c88f
ywonchae1 <ywonchae62@gmail.com>
2023-09-02 18:03:07 +0900

    Node.js 01-25 자바스크립트-함수

---
+++ Node.js/web2-nodejs/syntax/function2.js
@@ -0,0 +1,10 @@
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+
+function sum(first, second) {
+    // console.log(first + second);
+    console.log('a');
+    return first + second;
+    console.log('b');
+}
+console.log(sum(2, 4));
