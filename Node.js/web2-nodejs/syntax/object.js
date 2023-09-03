43b8278493346c39b992e85edeef1b253c3f910d
ywonchae1 <ywonchae62@gmail.com>
2023-09-03 11:45:16 +0900

    Node.js 01-40 자바스크립트-객체의 반복

--- Node.js/web2-nodejs/syntax/object.js
+++ Node.js/web2-nodejs/syntax/object.js
@@ -1,6 +1,11 @@
 const members = ['egoing', 'k8805', 'hoya'];
 console.log(members[1]);
 
+let i = 0;
+while(i < members.length) {
+    console.log('array loop', members[i]);
+    i = i + 1;
+}
 const roles = {
     'programmer': 'egoing',
     'designer': 'k8805',
@@ -8,3 +13,7 @@ const roles = {
 }
 console.log(roles.designer);
 console.log(roles['designer']);
+
+for(let name in roles) {
+    console.log('object =>', name, 'value =>', roles[name]);
+}
