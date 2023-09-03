74abb706954488da99acaf651fa1e01064637e1a
ywonchae1 <ywonchae62@gmail.com>
2023-09-03 10:58:20 +0900

    Node.js 01-31 App-글 생성 UI 만들기

--- Node.js/web2-nodejs/main.js
+++ Node.js/web2-nodejs/main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body) {
     <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
     </body>
     </html>
@@ -55,6 +56,24 @@ const app = http.createServer(function(request, response) {
                 });
             });
         }
+    } else if(pathname === '/create') {
+        fs.readdir('./data', function(error, filelist) {
+            const title = 'WEB - create';
+            const list = templateList(filelist);
+            const template = templateHTML(title, list, `
+            <form action="http://localhost:3000/create_process" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+            `);
+            response.writeHead(200);
+            response.end(template);
+        });
     } else {
         response.writeHead(404);
         response.end('Not found');
