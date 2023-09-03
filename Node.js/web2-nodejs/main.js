7eb9eed4cba8600278131aaa0ebc86fd08d6f06f
ywonchae1 <ywonchae62@gmail.com>
2023-09-03 12:19:16 +0900

    Node.js 01-44 App-모듈의 활용

--- Node.js/web2-nodejs/main.js
+++ Node.js/web2-nodejs/main.js
@@ -3,35 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
 
-const template = {
-    HTML: function(title, list, body, control) {
-        return `
-        <!doctype html>
-        <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            ${control}
-            ${body}
-        </body>
-        </html>
-        `;
-    },
-    list: function(filelist) {
-        let list = '<ul>';
-        let i = 0;
-        while(i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-        }
-        list = list + '</ul>';
-        return list;
-    }
-}
+const template = require('./lib/template.js');
 
 const app = http.createServer(function(request, response) {
     let _url = request.url;
