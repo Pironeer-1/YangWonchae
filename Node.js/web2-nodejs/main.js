233c75d19a2f023f81270befe9794df3e4a08cf5
ywonchae1 <ywonchae62@gmail.com>
2023-09-03 11:38:31 +0900

    Node.js 01-38 App-글 삭제 기능 구현

--- Node.js/web2-nodejs/main.js
+++ Node.js/web2-nodejs/main.js
@@ -137,6 +137,19 @@ const app = http.createServer(function(request, response) {
                 });
             });
         });
+    } else if(pathname === '/delete_process') {
+        let body = '';
+        request.on('data', function(data) {
+            body = body + data;
+        });
+        request.on('end', function() {
+            const post = qs.parse(body);
+            const id = post.id;
+            fs.unlink(`data/${id}`, function(error) {
+                response.writeHead(302, {Location: `/`});
+                response.end();
+            });
+        });
     } else {
         response.writeHead(404);
         response.end('Not found');
