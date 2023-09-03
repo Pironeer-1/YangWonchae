0c88e152a14d72340f25ea953c0fe0ecfada0bcf
ywonchae1 <ywonchae62@gmail.com>
2023-09-03 11:43:08 +0900

    Node.js 01-39 자바스크립트-객체의 형식

---
+++ Node.js/web2-nodejs/syntax/object.js
@@ -0,0 +1,10 @@
+const members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]);
+
+const roles = {
+    'programmer': 'egoing',
+    'designer': 'k8805',
+    'manager': 'hoya'
+}
+console.log(roles.designer);
+console.log(roles['designer']);
