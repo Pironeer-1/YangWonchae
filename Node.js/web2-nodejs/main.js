ae76addbf62dd9838e82324ef998e43e54c442a8
ywonchae1 <ywonchae62@gmail.com>
2023-09-03 11:36:08 +0900

    Node.js 01-37 App-글 삭제(삭제 버튼 구현)

--- Node.js/web2-nodejs/main.js
+++ Node.js/web2-nodejs/main.js
@@ -56,7 +56,12 @@ const app = http.createServer(function(request, response) {
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(error, description) {
                     const template = templateHTML(title, list,
                         `<h2>${title}</h2><p>${description}</p>`,
-                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                        `<a href="/create">create</a>
+                        <a href="/update?id=${title}">update</a>
+                        <form action="/delete_process" method="post" onsubmit="return confirm('정말로 삭제하시겠습니까?');">
+                            <input type="hidden" name="id" value=${title}>
+                            <input type="submit" value="delete">
+                        </form>`
                         );
                     response.writeHead(200);
                     response.end(template);
