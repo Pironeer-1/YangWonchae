87fa5420ac71c611c81ba6d1ba590812642a870c
ywonchae1 <ywonchae62@gmail.com>
2023-09-03 11:25:58 +0900

    Node.js 01-35 App-글 수정(수정할 정보 전송)

--- Node.js/web2-nodejs/main.js
+++ Node.js/web2-nodejs/main.js
@@ -68,7 +68,7 @@ const app = http.createServer(function(request, response) {
             const title = 'WEB - create';
             const list = templateList(filelist);
             const template = templateHTML(title, list, `
-            <form action="http://localhost:3000/create_process" method="post">
+            <form action="/create_process" method="post">
                 <p><input type="text" name="title" placeholder="title"></p>
                 <p>
                     <textarea name="description" placeholder="description"></textarea>
@@ -93,6 +93,26 @@ const app = http.createServer(function(request, response) {
             fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
                 response.writeHead(302, {Location: `/?id=${title}`});
                 response.end();
+            });
+        });
+    } else if(pathname === '/update') {
+        fs.readdir('./data', function(error, filelist) {
+            const list = templateList(filelist);
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(error, description) {
+                const template = templateHTML(title, list,
+                    `<form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="description">${description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                    `, '');
+                response.writeHead(200);
+                response.end(template);
             });
         });
     } else {
