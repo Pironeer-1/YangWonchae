92abf40583a6e055600a3428ee020657c2637033
ywonchae1 <ywonchae62@gmail.com>
2023-09-03 23:03:08 +0900

    Node.js 02-03 mysql 모듈의 기본 사용법

---
+++ Node.js/node.js-mysql-1/nodejs/mysql.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql2');
+var connection = mysql.createPool({
+    host: 'localhost',
+    user: 'ywonchae1',
+    password: 'ywonchae1',
+    database: 'opentutorials',
+    port: 3306
+});
+
+// connection.connect();
+connection.query('SHOW DATABASES;', function (error, results) {
+    console.log(results);
+});
+connection.query('SELECT * FROM topic', function (error, results, fields) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+});
+
+// connection.end();
