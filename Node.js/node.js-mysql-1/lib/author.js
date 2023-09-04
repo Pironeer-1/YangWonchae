b74db68e444f536230ee55ae43500803e803df48
ywonchae1 <ywonchae62@gmail.com>
2023-09-04 23:23:05 +0900

    Node.js 02-18 저자 수정 기능 구현

--- Node.js/node.js-mysql-1/lib/author.js
+++ Node.js/node.js-mysql-1/lib/author.js
@@ -56,6 +56,69 @@ module.exports = {
                 }
                 response.writeHead(302, {Location: `/author`});
                 response.end();
+            });
+        });
+    },
+    update: function(request, response) {
+        db.query(`SELECT * FROM topic`, function (error, topics) {
+            db.query(`SELECT * FROM author`, function(error2, authors) {
+                const _url = request.url;
+                const queryData = url.parse(_url, true).query;
+                db.query(`SELECT * FROM author WHERE id=?`, [queryData.id], function(error3, author) {
+                    const title = 'author';
+                    const list = template.list(topics);
+                    const html = template.HTML(title, list,
+                        `
+                        ${template.authorTable(authors)}
+                        <style>
+                            table {
+                                border-collapse: collapse;
+                            }
+                            td {
+                                border: 1px solid black;
+                            }
+                        </style>
+                        <form action="/author/update_process" method="post">
+                            <p>
+                                <input type="hidden" name="id" value=${author[0].id}>
+                            </p>
+                            <p>
+                                <input type="text" name="name" placeholder="name" value=${author[0].name}>
+                            </p>
+                            <p>
+                                <textarea name="profile" placeholder="description">${author[0].profile}</textarea>
+                            </p>
+                            <p>
+                                <input type="submit" value="update">
+                            </p>
+                        </form>
+                        `,
+                        ``
+                    );
+                    response.writeHead(200);
+                    response.end(html);
+                });
+            });
+        });
+    },
+    update_process: function(request, response) {
+        let body = '';
+        request.on('data', function(data) {
+            body += data;
+        });
+        request.on('end', function() {
+            const post = qs.parse(body);
+            db.query(`
+            UPDATE author
+            SET name=?, profile=?
+            WHERE id=?`,
+            [post.name, post.profile, post.id],
+            function(error, result) {
+                if(error) {
+                    throw error;
+                }
+                response.writeHead(302, {Location: `/author`});
+                response.end();
             })
         })
     }
