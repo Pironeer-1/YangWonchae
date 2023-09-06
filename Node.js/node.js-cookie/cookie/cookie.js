dbbea01dedb073a287ad6c1bc2ffa1fa1e255171
ywonchae1 <ywonchae62@gmail.com>
2023-09-06 17:49:16 +0900

    Node.js 04-04 쿠키 읽기

--- Node.js/node.js-cookie/cookie/cookie.js
+++ Node.js/node.js-cookie/cookie/cookie.js
@@ -1,7 +1,14 @@
 const http = require('http');
+const cookie = require('cookie');
 http.createServer(function(request, response) {
-    // response.writeHead(200, {
-    //     'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
-    // });
+    console.log(request.headers.cookie);
+    let cookies = {};
+    if(request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
+    response.writeHead(200, {
+        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    });
     response.end('Cookie!!');
 }).listen(3000);
