f74f73ac44659a76ea921811c60543e2ca2f3531
ywonchae1 <ywonchae62@gmail.com>
2023-09-06 18:01:04 +0900

    Node.js 04-07 쿠키 옵션(Secure와 HttpOnly)

--- Node.js/node.js-cookie/cookie/cookie.js
+++ Node.js/node.js-cookie/cookie/cookie.js
@@ -8,7 +8,13 @@ http.createServer(function(request, response) {
     }
     console.log(cookies);
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry', `Permanent=cookies; Max-Age=${60*60*24*30}`]
+        'Set-Cookie': [
+            'yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}`,
+            'Secure=Secure; Secure',
+            'HttpOnly=HttpOnly; HttpOnly'
+        ]
     });
     response.end('Cookie!!');
 }).listen(3000);
