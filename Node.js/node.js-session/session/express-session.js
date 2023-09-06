b630f9e4c460d8b216df47811ddc039f4a6dc95c
ywonchae1 <ywonchae62@gmail.com>
2023-09-06 21:46:43 +0900

    Node.js 05-03 express-session의 옵션

--- Node.js/node.js-session/session/express-session.js
+++ Node.js/node.js-session/session/express-session.js
@@ -10,26 +10,8 @@ app.use(session({
   saveUninitialized: true
 }))
 
-app.use(function (req, res, next) {
-  if (!req.session.views) {
-    req.session.views = {}
-  }
-
-  // get the url pathname
-  var pathname = parseurl(req).pathname
-
-  // count the views
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-
-  next()
-})
-
-app.get('/foo', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
-})
-
-app.get('/bar', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+app.get('/', function (req, res, next) {
+  res.send('Hello session')
 })
 
 app.listen(3000)
