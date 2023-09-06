78509de3550cb5aee136e73ebd54a25db1a12068
ywonchae1 <ywonchae62@gmail.com>
2023-09-06 11:57:06 +0900

    Node.js 03-09 익스프레스 미들웨어 사용하기

--- Node.js/node.js-express/main.js
+++ Node.js/node.js-express/main.js
@@ -143,13 +143,19 @@ var app = http.createServer(function(request,response){
 });
 app.listen(3000);
 */
-var fs = require('fs');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
+const fs = require('fs');
+const template = require('./lib/template.js');
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
+const qs = require('querystring');
+const bodyParser = require('body-parser');
+const compression = require('compression');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
 
 app.get('/', function (request, response) {
     fs.readdir('./data', function(error, filelist){
@@ -177,8 +183,8 @@ app.get('/page/:pageId', function (request, response) {
           const html = template.HTML(sanitizedTitle, list,
             `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
             ` <a href="/create">create</a>
-              <a href="/update?id=${sanitizedTitle}">update</a>
-              <form action="delete_process" method="post">
+              <a href="/update/${sanitizedTitle}">update</a>
+              <form action="/delete_process" method="post">
                 <input type="hidden" name="id" value="${sanitizedTitle}">
                 <input type="submit" value="delete">
               </form>`
@@ -208,20 +214,63 @@ app.get('/create', function(request, response) {
 });
 
 app.post('/create_process', function(request, response) {
-    const body = '';
-    request.on('data', function(data){
-        body = body + data;
+    const post = request.body;
+    const title = post.title;
+    const description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.writeHead(302, {Location: `/page/${title}`});
+        response.end();
     });
-    request.on('end', function(){
-        const post = qs.parse(body);
-        const title = post.title;
-        const description = post.description;
+});
+
+app.get('/update/:pageId', function (request, response) {
+    fs.readdir('./data', function(error, filelist){
+        const filteredId = path.parse(request.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+          const title = request.params.pageId;
+          const list = template.list(filelist);
+          const html = template.HTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+          );
+          response.send(html);
+        });
+    });
+});
+
+app.post('/update_process', function(request, response) {
+    const post = request.body;
+    const id = post.id;
+    const title = post.title;
+    const description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function(error) {
         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
+            response.writeHead(302, {Location: `/page/${title}`});
             response.end();
         });
     });
-})
+});
+
+app.post('/delete_process', function(request, response) {
+    const post = request.body;
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function(error){
+    response.writeHead(302, {Location: `/`});
+    response.end();
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
