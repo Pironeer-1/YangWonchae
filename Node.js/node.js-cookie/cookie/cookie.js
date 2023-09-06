4f156f2fec61ca5a35fd7b8e9084f22996061d28
ywonchae1 <ywonchae62@gmail.com>
2023-09-06 17:53:44 +0900

    Node.js 04-06 세션(Session)과 영구 쿠키(Permanent 쿠키)

--- Node.js/node.js-cookie/cookie/cookie.js
+++ Node.js/node.js-cookie/cookie/cookie.js
@@ -8,7 +8,7 @@ http.createServer(function(request, response) {
     }
     console.log(cookies);
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry', `Permanent=cookies; Max-Age=${60*60*24*30}`]
     });
     response.end('Cookie!!');
 }).listen(3000);
