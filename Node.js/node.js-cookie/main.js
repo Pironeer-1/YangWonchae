25c4a2a49ea0421b2565ff10236befe1ba576fc0
ywonchae1 <ywonchae62@gmail.com>
2023-09-06 21:08:41 +0900

    Node.js 04-09 쿠키를 이용한 인증 기능 구현

--- Node.js/node.js-cookie/main.js
+++ Node.js/node.js-cookie/main.js
@@ -5,11 +5,33 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+const cookie = require('cookie');
+
+function authIsOwner(request, response) {
+  let isOwner = false;
+  let cookies = {};
+
+  if(request.headers.cookie) {
+    cookies = cookie.parse(request.headers.cookie);
+  }
+  if(cookies.email === 'egoing777@gmail.com' && cookies.password === '111111') {
+    isOwner = true;
+  }
+  return isOwner;
+}
+function authStatusUI(request, response) {
+  let authStatusUI = '<a href="/login">login</a>';
+  if(authIsOwner(request, response)) {
+    authStatusUI = `<a href="/logout_process">logout</a>`;
+  }
+  return authStatusUI;
+}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
+
     if(pathname === '/'){
       if(queryData.id === undefined){
         fs.readdir('./data', function(error, filelist){
@@ -18,7 +40,8 @@ var app = http.createServer(function(request,response){
           var list = template.list(filelist);
           var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
+            `<a href="/create">create</a>`,
+            authStatusUI(request, response)
           );
           response.writeHead(200);
           response.end(html);
@@ -40,7 +63,8 @@ var app = http.createServer(function(request,response){
                 <form action="delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
-                </form>`
+                </form>`,
+              authStatusUI(request, response)
             );
             response.writeHead(200);
             response.end(html);
@@ -61,11 +85,15 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, '');
+        `, '', authStatusUI(request, response));
         response.writeHead(200);
         response.end(html);
       });
     } else if(pathname === '/create_process'){
+      if(authIsOwner(request, response) === false) {
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -98,13 +126,18 @@ var app = http.createServer(function(request,response){
               </p>
             </form>
             `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+            authStatusUI(request, response)
           );
           response.writeHead(200);
           response.end(html);
         });
       });
     } else if(pathname === '/update_process'){
+      if(authIsOwner(request, response) === false) {
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -118,10 +151,14 @@ var app = http.createServer(function(request,response){
             fs.writeFile(`data/${title}`, description, 'utf8', function(err){
               response.writeHead(302, {Location: `/?id=${title}`});
               response.end();
-            })
+            });
           });
       });
     } else if(pathname === '/delete_process'){
+      if(authIsOwner(request, response) === false) {
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -134,6 +171,64 @@ var app = http.createServer(function(request,response){
             response.writeHead(302, {Location: `/`});
             response.end();
           })
+      });
+    } else if(pathname === '/login') {
+      fs.readdir('./data', function(error, filelist){
+        var title = 'Login';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list,
+          `<form action="login_process" method="post">
+            <p><input type="text" name="email" placeholder="email"></p>
+            <p><input type="password" name="password" placeholder="password"></p>
+            <p><input type="submit"></p>
+          </form>`,
+          `<a href="/create">create</a>`
+        );
+        response.writeHead(200);
+        response.end(html);
+      });
+    } else if(pathname === '/login_process') {
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+        const post = qs.parse(body);
+        if(post.email === 'egoing777@gmail.com' && post.password === '111111') {
+          isOwner = true;
+          response.writeHead(302, {
+            'Set-Cookie': [
+              `email=${post.email}`,
+              `password=${post.password}`,
+              `nickname=egoing`
+            ],
+            Location: `/`
+          });
+          response.end();
+        } else {
+          response.end('Who?');
+        }
+      });
+    } else if(pathname === '/logout_process') {
+      if(authIsOwner(request, response) === false) {
+        response.end('Login required!!');
+        return false;
+      }
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+        const post = qs.parse(body);
+        response.writeHead(302, {
+          'Set-Cookie': [
+            `email=; Max-Age=0`,
+            `password=; Max-Age=0`,
+            `nickname=; Max-Age=0`,
+          ],
+          Location: `/`
+        });
+        response.end();
       });
     } else {
       response.writeHead(404);
