754d012413c72ed49ff986964d168b4097ce461e
ywonchae1 <ywonchae62@gmail.com>
2023-09-07 10:29:01 +0900

    Node.js 06-02 Passport.js 설치

---
+++ Node.js/node.js-passport/main_.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var sanitizeHtml = require('sanitize-html');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet');
+app.use(helmet());
+
+var indexRouter = require('./routes/index');
+var topicRouter = require('./routes/topic');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.use(express.static('public'));
+app.get('*', function(request, response, next) {
+    fs.readdir('./data', function(error, filelist) {
+        request.list = filelist;
+        next();
+    });
+});
+
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
+});
